9a460d61eec8b9ab094ab374a41c95fda90d20d9
Gentry Rolofson <xdarkicex@gmail.com>
2016-05-19 23:00:20 -0700

    Bull Shit Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # app/views/application/index.html.haml
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def index
+  end
+  protected
+  def configure_permitted_parameters
+    [:account_update, :sign_up].each do |action|
+      devise_parameter_sanitizer.for(action) << [:avatar, :bio, :skills, :name, :job_title, :portfolio]
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,26 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Paperclip
+  has_mongoid_attached_file :avatar, :styles => { :medium =>"500x500>", :thumb => "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  field :name, type: String
+  field :jobtitle, type: String
+  field :skills, type: String
+  field :gender, type: String
+  field :email, type: String
+  field :phone, type: String
+  field :bio, type: String
+
+
+  # has_many :posts, dependent: :destroy
+
+  validates_presence_of :name, :message => "Can't be blank"
+  validates :name, length:{maximum: 15}
+  validates_presence_of :email, :message => "Can't be blank"
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
